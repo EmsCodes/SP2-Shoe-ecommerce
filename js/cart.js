0c32424d6ab8cd3eca89f045a6b9d74b0ed786a4
EmsCodes <69721898+EmsCodes@users.noreply.github.com>
2021-11-30 20:46:41 +0100

    cart.js
    delete cart item from cart and local storage

--- js/cart.js
+++ js/cart.js
@@ -1,29 +1,66 @@
-import { getFromStorage } from "./components/storage.js";
+import { getFromStorage, saveToStorage } from "./components/storage.js";
 
-(function getCart() {
-	const cartList = document.querySelector(".shopping-list");
+let cart = getFromStorage("cart");
+const cartList = document.querySelector(".shopping-list");
 
-	const cart = getFromStorage("cart");
-
-	// console.log(cart);
-
+function getCart(cartItems) {
 	let totalPrice = 0;
 
-	cart.forEach((item) => {
+	cartList.innerHTML = "";
+
+	cartItems.forEach((item) => {
 		const price = parseInt(item.productPrice);
+
+		const itemId = item.productId;
 
 		totalPrice += price;
 
 		cartList.innerHTML += `<li class="cart-item"> 
       <img src="${item.productImage}" class="product-img">
       <div>
-      <p class="productBrand">Brand</p>
-      <h3>${item.productTitle}</h3>
-      <p>Size: 10</p>
-      <p>Colour: Brown</p>
+        <p class="productBrand">Brand</p>
+        <h3>${item.productTitle}</h3>
+        <p>Size: 10</p>
+        <p>Colour: Brown</p>
       </div>
+      <div class="cart-info">
+        <p class="quantity">${item}</p>
+        <p class="price">${item.productPrice}</p>
+        <p class="total-price">${item.productPrice}</p>
+      </div>
+      <button class="remove-btn" data-id="${itemId}"><i class="fas fa-times" data-id="${itemId}"></i></button>
     </li>`;
+
+		if ((item = 0)) {
+			cartList.innerHTML += "Empty cart!";
+		}
+
+		const removeBtn = document.querySelectorAll(".remove-btn");
+
+		removeBtn.forEach((btn) => {
+			btn.addEventListener("click", removeItem);
+		});
 	});
 
-	console.log(totalPrice);
-})();
+	// console.log(totalPrice);
+}
+
+function removeItem(event) {
+	const id = event.target.dataset.id;
+
+	const currentCart = getFromStorage("cart");
+
+	let newCart = currentCart.filter(function (product) {
+		console.log(product);
+
+		if (product.productId !== id) {
+			return true;
+		}
+	});
+	console.log(newCart);
+
+	getCart(newCart);
+	saveToStorage("cart", newCart);
+}
+
+getCart(cart);
